0258ce4efae9f91a7d3e424d8ba88fc62c7734f5
ShuraDE <mail@kev-in.net>
2015-05-14 12:24:15 +0200

    Update main.cpp

--- CPBO/main.cpp
+++ CPBO/main.cpp
@@ -16,8 +16,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA  02110-1301  USA
 */
 
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <cstring>
 #include <cstdlib>
 #include "pbo.h"
 
