a71b240701d2d5ef48a9778951ed799bc628db8b
ShuraDE <mail@kev-in.net>
2015-05-14 12:23:12 +0200

    boost libary -> filesystem eingebunden

--- CPBO/pbo.cpp
+++ CPBO/pbo.cpp
@@ -17,26 +17,17 @@ MA  02110-1301  USA
 */
 
 // PBO functionality
+
 #include "pbo.h"
 #include "sha1.h"
-#include <time.h>
-#include <string.h>
+#include <iostream>
+#include <cstring>
+#include <iterator>
+#include <algorithm>
+#include <boost/filesystem.hpp>
 
-
-// From the internets
-void UnixTimeToFileTime(time_t t, LPFILETIME pft) {
-     LONGLONG ll;
-     ll = Int32x32To64(t, 10000000) + 116444736000000000;
-     pft->dwLowDateTime = (DWORD)ll;
-     pft->dwHighDateTime = (DWORD) (ll >> 32);
-}
-time_t FILETIMEToUnixTime(FILETIME filetime) {
-  long long int t = filetime.dwHighDateTime;
-    t <<= 32;
-    t += (unsigned long)filetime.dwLowDateTime;
-    t -= 116444736000000000LL;
-  return (time_t) (t / 10000000);
-}
+//using namespace std;
+using namespace boost::filesystem;
 
 
 // Does file exist?
@@ -45,350 +36,92 @@ bool fileExists(char *filename) {
   FILE *f;
   errno_t errOpenFile = fopen_s(&f, filename, "rb");
   if(f) {
-    fclose(f);
-    return true;
+	fclose(f);
+	return true;
   }
   return false;
 }
-
-// Read null terminated string from file
-bool fgetsz(void *d, int maxlen, FILE *f) {
-  int r = 0;
-  char *dd = (char*) d;
-  char c;
-  do {
-    c = fgetc(f);
-    *dd++ = c;
-    r++;
-  } while(c != 0x00 && c != EOF && r<maxlen);
-  if(ferror(f))
-    return false;
-  return true;
-}
-
-/*
-// Create all subdirs in filename
-void createDirs(char *fname) {
-  char *fn = new char[strlen(fname)+1];
-  strcpy(fn, fname);
-
-  for(DWORD i=0;i<strlen(fn);i++)
-    if(fn[i] == '/' || fn[i] == '\\') {
-      fn[i] = 0x00;
-      CreateDirectory(fn, NULL);
-      fn[i] = '/';
-    }
-
-  delete[] fn;
-}
-
-// Extract a PBO, sf = source filename, dd = target directory
-bool pboEx(char *sf, char *dd, bool overwrite, bool gui) {
-  FILE *i = fopen(sf, "rb");
-  if(!i) 
-    return false;	
-
-  int status = STATUS_HEADER;
-  int ftableptr = 0;
-  int numFiles = 0;
-  char str[FNAMELEN];
-
-  FTENTRY *ft = NULL;
-  int fi = 0;
-
-  char prefix[FNAMELEN];
-  char product[FNAMELEN];
-  memset(prefix, 0, FNAMELEN);
-  memset(product, 0, FNAMELEN);
-
-  bool someFailed = false;
-
-  fgetsz(str, FNAMELEN, i);
-
-  if(strlen(str) != 0) {
-    // No header block - just file table
-    status = STATUS_FILECOUNT;
-    ftableptr = 0;
-    fseek(i, 0, SEEK_SET);
-  }
-  //printf("str: <%s> %d\n", str, strlen(str));
-
-  // Read
-  while(status != STATUS_DONE) {
-    fgetsz(str, FNAMELEN, i);
-    //printf("str: <%s>\n", str);
-    switch(status) {
-      case STATUS_HEADER:
-        // Reading the first header blocks...
-        if(strlen(str) != 0) {
-          char *foo = str;
-
-          if(!strcmp(foo, "sreV")) {
-            // "Vers" block, nothing useful here...
-            fseek(i, 15, SEEK_CUR);
-          } else if(!strcmp(foo, "product")) {
-            // "ProducT" block, nothing we need here either						
-            fgetsz(product, FNAMELEN, i);
-          } else if(!strcmp(foo, "prefix")) {
-            // PBO filename prefix
-            fgetsz(prefix, FNAMELEN, i);
-          } else if(!strcmp(foo, "version")) {
-            // PBO version?
-            char version[FNAMELEN];
-            fgetsz(version, FNAMELEN, i);
-          } else if(!strcmp(foo, "svn")) {
-            // SVN for vbs2 pbos
-            char joop[FNAMELEN];
-            fgetsz(joop, FNAMELEN, i);
-          } else if(strlen(foo) != 0) {
-            // Unknown block
-            //printf("pboEx: Unknown block in header! aborting <%s>\n", foo);
-            printf("pboEx: Warning: Unknown block in header: <%s>\n", foo);
-            //return false;
-            char joop[FNAMELEN];
-            fgetsz(joop, FNAMELEN, i);
-          }
-
-          ftableptr = ftell(i)+1;
-          break;
-        }
-        // str isn't null, we reached end of headers
-        status = STATUS_FILECOUNT;
-        break;
-
-      case STATUS_FILECOUNT:
-        // Count number of files
-        if(strlen(str) != 0) {
-          PBOENTRY e;
-          fread(&e, sizeof(e), 1, i);
-          numFiles++;
-        } else {
-          // End of file table, seek back to beginning of table
-          fseek(i, ftableptr, SEEK_SET);
-          printf("Found %d files\n", numFiles);
-          status = STATUS_FILETABLE;
-
-          // Allocate memory for filetable
-          ft = new FTENTRY[numFiles];
-        }
-        break;
-
-      case STATUS_FILETABLE:
-        if(strlen(str) != 0) {
-          // Read file table block, filename is already read
-          PBOENTRY e;
-          fread(&e, sizeof(e), 1, i);
-
-          //printf("File: %s, %d KB\n", str, e.DataSize/1024);
-         
-		// if(e.PackingMethod == 0x43707273) {
-        //    // Cant handle these (yet?)
-        //    
-        //    printf("%s:\n", str);
-        //    printf("%d\n", e.DataSize);
-        //   printf("%d\n", e.OriginalSize);
-
-        //    printf("Compressed file found! aborting\n");
-        //    return false;
-        //  }
-          
-          if(e.PackingMethod == 0x43707273)
-            ft[fi].origsize = e.OriginalSize;
-          else
-            ft[fi].origsize = 0;
-
-          strcpy(ft[fi].fname, str);
-          ft[fi].len = e.DataSize;
-          ft[fi].timestamp = e.TimeStamp;
-          ft[fi].extract = true;
-
-          fi++;
-
-          break;
-        }
-        
-        status = STATUS_DONE;
-
-        // Seek over last entry
-        PBOENTRY e;
-        fread(&e, sizeof(e), 1, i);
-        break;
-    }
-  }
-
-  // Get output dir name
-  char outdir[FNAMELEN];
-  memset(outdir, 0, FNAMELEN);
-  if(strlen(dd) == 0) {
-    if(!strstr(sf, ":")) {
-      GetCurrentDirectory(512, outdir);
-      strcat(outdir, "\\");
-      strncat(outdir, sf, strlen(sf)-4);
-    } else
-      strncpy(outdir, sf, strlen(sf)-4);
-  } else
-    strcpy(outdir, dd);
-
-#ifdef WITH_GUI
-  if(gui) {
-    hideWindow(true);
-    ftg = ft;
-    ftgsize = numFiles;
-    doutdir = outdir;
-    dinfile = sf;
-    int r = DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_EXTRACTDIALOG), NULL, dialogproc);
-    hideWindow(false);
-    if(r == IDCANCEL) {
-      printf("cancelled\n");
-      return true;
-    }
-  }
-#endif
-
-  // Ask for overwriting
-  if(!overwrite && !CreateDirectory(outdir, NULL)) {
-    char str[256];
-    sprintf(str, "Directory '%s' already exists.", outdir);
-    return true; // Abort cleanly
-  }
-
-  // Create prefix store file
-  if(strlen(prefix) > 0) {
-    char oname[FNAMELEN];
-    sprintf(oname, "%s\\%s", outdir, PREFIXFILE);
-    createDirs(oname);
-    FILE *fo = fopen(oname, "wb");
-    fputs(prefix ,fo);
-    fclose(fo);
-  }
-
-  // Extract files
-  for(int o=0;o<numFiles;o++) {
-    char oname[FNAMELEN];
-    sprintf(oname, "%s\\%s", outdir, ft[o].fname);
-
-    if(!ft[o].extract)  {
-      fseek(i, ft[o].len, SEEK_CUR); 
-      continue;
-    }
-
-    int l = ft[o].len;
-    printf("Extracting: %s (%d KB)\n", ft[o].fname, ft[o].len/1024);
-
-    createDirs(oname);
-    FILE *fo = fopen(oname, "wb");
-
-    if(ft[o].origsize == 0) {
-      // Read from pbo, write to file				
-      #define BUFSIZE	1024*1024*4
-      char *buf = new char[BUFSIZE];
-      int w = l;
-      while(w > 0) {
-        int read = fread(buf, 1, w>BUFSIZE?BUFSIZE:w, i);
-        fwrite(buf, read, 1, fo);
-        w-=BUFSIZE;			
-      }
-      delete[] buf;
-    } else {
-      // Compressed file, read it all to memory & decompress
-      // TODO: with large files this will use alot of memory...
-      BYTE *buf = new BYTE[l];
-      BYTE *outbuf = new BYTE[ft[o].origsize+32];
-      fread(buf, 1, l, i);
-      bool ret = pboDecompress(buf, outbuf, l, ft[o].origsize);
-
-      fwrite(outbuf, ft[o].origsize, 1, fo);
-      
-      delete[] buf;
-      delete[] outbuf;
-
-      if(!ret) {
-        // Checksum failed
-        //return false;
-        someFailed = true;
-      }
-      printf("Decompressed %dKB -> %dKB\n", l/1024, ft[o].origsize/1024);
-    }
-
-    fclose(fo);
-
-    // Set file time
-    if(ft[o].timestamp != 0) {			
-      HANDLE tf = CreateFile(oname, GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
-      if(tf != INVALID_HANDLE_VALUE) {
-        FILETIME filet;
-        UnixTimeToFileTime(ft[o].timestamp, &filet);
-        SetFileTime(tf, &filet, NULL, NULL);
-        CloseHandle(tf);
-      }
-    } else {
-      printf("Warning! File creation time not set (Invalid time)\n");
-    }
-  }
-
-  fclose(i);
-  delete[] ft;
-
-  return !someFailed;
-}
-*/
 
 
 int getDirFiles(char *sd, FTENTRY *ftable, int *fti, char excludes[EX_NUM][EX_LEN]) {
   char dir[FNAMELEN];
   sprintf(dir, "%s\\*.*", sd);
 
-  WIN32_FIND_DATA fd;
   int res = 1;
   int count = 0;
-  HANDLE h;
-  for(h=FindFirstFile(dir, &fd);res && h != INVALID_HANDLE_VALUE;res = FindNextFile(h, &fd)) {
-    if(!strcmp(fd.cFileName,".."))
-      continue;
-    if(!strcmp(fd.cFileName,"."))
-      continue;
-    if(!_stricmp(fd.cFileName, PREFIXFILE)) // Do not pack prefix file
-      continue;
-    if(!_stricmp(fd.cFileName, EXCLUDEFILE))
-      continue;
 
-    if((fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
-      char foo[1024];
-      sprintf(foo, "%s\\%s", sd, fd.cFileName);
-      count += getDirFiles(foo, ftable, fti, excludes);
-    } else {
-      // Check for exclude
-      bool skip = false;
-      for(int i=0;i<EX_NUM;i++) {
-        if(strlen(excludes[i]) > 1 && !_stricmp(excludes[i], &fd.cFileName[strlen(fd.cFileName)-strlen(excludes[i])])) {
-          // printf("Skipping: %s - %s\n", fd.cFileName, excludes[i]);
-          skip = true;
-          break;
-        }
-      }
-      if(skip)
-        continue; // File extension is excluded
+  path p (dir);
 
-      count++;
-      if(ftable != NULL) {
-        // Fill table. filename...
-        static char foo[1024];
-        sprintf(foo, "%s\\%s", sd, fd.cFileName);
-        strcpy(ftable[*fti].fname, foo);
+  try 
+  {
+	  if (exists(p)) 
+	  {
+		  if (is_directory(p)) 
+		  {
 
-        // Modification time
-        ftable[*fti].timestamp = (DWORD) FILETIMEToUnixTime(fd.ftCreationTime);
+			  for (path::iterator it = p.begin(); it != p.end(); ++it) 
+			  {
+				  path fn = it->filename();
 
-        // Size, TODO: 4GB limit check?
-        ftable[*fti].len = (fd.nFileSizeHigh * MAXDWORD) + fd.nFileSizeLow; 
 
-        (*fti)++;
-      }
-    }
+				  if(!strcmp(fn.string().c_str(),".."))
+					  continue;
+					if(!strcmp(fn.string().c_str(),"."))
+					  continue;
+					if(!_stricmp(fn.string().c_str(), PREFIXFILE)) // Do not pack prefix file
+					  continue;
+					if(!_stricmp(fn.string().c_str(), EXCLUDEFILE))
+					  continue;
+
+					if(is_directory((path)*it)) {
+						  char foo[1024];
+						  sprintf(foo, "%s\\%s", sd, it->filename());
+						  count += getDirFiles(foo, ftable, fti, excludes);
+					} else {
+						  // Check for exclude
+						  bool skip = false;
+						  for(int i=0;i<EX_NUM;i++) {
+							if(strlen(excludes[i]) > 1 && !_stricmp(excludes[i], &fn.string().c_str()[strlen(fn.string().c_str())-strlen(excludes[i])])) {
+							  // printf("Skipping: %s - %s\n", fd.cFileName, excludes[i]);
+							  skip = true;
+							  break;
+							}
+						  }
+						  if(skip)
+							continue; // File extension is excluded
+
+						  count++;
+						  if(ftable != NULL) {
+							// Fill table. filename...
+							static char foo[1024];
+							sprintf(foo, "%s\\%s", sd, it->filename());
+							strcpy(ftable[*fti].fname, foo);
+
+							// Modification time
+//WARNING               original mit creation time
+//							ftable[*fti].timestamp = (DWORD) FILETIMEToUnixTime(fd.ftCreationTime);
+							ftable[*fti].timestamp = static_cast<long int>(last_write_time(p));
+
+							// Size, TODO: 4GB limit check?
+							//ftable[*fti].len = (fd.nFileSizeHigh * MAXDWORD) + fd.nFileSizeLow; 
+//INCOMPLETE
+							ftable[*fti].len = file_size(p); 
+
+							(*fti)++;
+						  }
+					}
+			  }
+
+
+			  return count;
+
+		  }
+	  }
   }
-  if(h != INVALID_HANDLE_VALUE)
-    FindClose(h);
-  return count;
+  catch (const filesystem_error& ex)
+  {
+	  printf(ex.what());
+  }
 }
 
 // Create a PBO, sd = source directory, df = target file
@@ -401,23 +134,23 @@ bool pboPack(char *sd, char *df, bool overwrite) {
   FILE *ef = fopen(exname, "rb");
   int eidx = 0;
   if(ef) {
-    printf("Excluded: ");
-    while(fgets(excludes[eidx], EX_LEN, ef)) {			
-      // Strip line feed
-      if(excludes[eidx][strlen(excludes[eidx])-2] == 0x0D) // DOS
-        excludes[eidx][strlen(excludes[eidx])-2] = 0x00;
-      if(excludes[eidx][strlen(excludes[eidx])-1] == 0x0A) // Unix
-        excludes[eidx][strlen(excludes[eidx])-1] = 0x00;
-      printf("<%s> ", excludes[eidx]);
-      eidx++;
-    }
-    printf("\n");
-    fclose(ef);
+	printf("Excluded: ");
+	while(fgets(excludes[eidx], EX_LEN, ef)) {			
+	  // Strip line feed
+	  if(excludes[eidx][strlen(excludes[eidx])-2] == 0x0D) // DOS
+		excludes[eidx][strlen(excludes[eidx])-2] = 0x00;
+	  if(excludes[eidx][strlen(excludes[eidx])-1] == 0x0A) // Unix
+		excludes[eidx][strlen(excludes[eidx])-1] = 0x00;
+	  printf("<%s> ", excludes[eidx]);
+	  eidx++;
+	}
+	printf("\n");
+	fclose(ef);
   }
 
   int c = getDirFiles(sd, NULL, NULL, excludes); // Get number of files
   if(c == 0)
-    return false;
+	return false;
   printf("%d files\n", c);
 
   // Allocate file table & fill it
@@ -429,21 +162,21 @@ bool pboPack(char *sd, char *df, bool overwrite) {
   // Open output file and create header
   char outname[FNAMELEN];
   if(strlen(df) != 0)
-    sprintf(outname, "%s", df);
+	sprintf(outname, "%s", df);
   else
-    sprintf(outname, "%s.pbo", sd);
+	sprintf(outname, "%s.pbo", sd);
 
   // Ask for overwriting
   if(!overwrite && fileExists(outname)) {
-    char str[256];
-    sprintf(str, "File %s already exists", outname);
-    return true; // Abort cleanly
+	char str[256];
+	sprintf(str, "File %s already exists", outname);
+	return true; // Abort cleanly
   }
 
   FILE *o = fopen(outname, "w+b");
   if(!o) {
-    printf("Unable to open %s for writing!\n", outname);
-    return false;
+	printf("Unable to open %s for writing!\n", outname);
+	return false;
   }
 
   // Prepare SHA-1
@@ -461,32 +194,32 @@ bool pboPack(char *sd, char *df, bool overwrite) {
   sprintf(foo, "%s\\%s", sd, PREFIXFILE);
   FILE *hf = fopen(foo, "rb");
   if(hf) {
-    char prefix[FNAMELEN];
-    fgets(prefix, FNAMELEN, hf);
-    fclose(hf);
-    fputs("prefix", o);
-    fputc(0x00, o);
-    fputs(prefix, o);
-    fputc(0x00, o);
-    fputc(0x00, o);
-    printf("prefix: %s\n", prefix);
+	char prefix[FNAMELEN];
+	fgets(prefix, FNAMELEN, hf);
+	fclose(hf);
+	fputs("prefix", o);
+	fputc(0x00, o);
+	fputs(prefix, o);
+	fputc(0x00, o);
+	fputc(0x00, o);
+	printf("prefix: %s\n", prefix);
   } else {
-    fputc(0x00, o); // Header terminator
+	fputc(0x00, o); // Header terminator
   }
 
   // Write file table
   for(int i=0;i<fti;i++) {
-    fputs(ft[i].fname+strlen(sd)+1, o);
-    fputc(0x00, o);
+	fputs(ft[i].fname+strlen(sd)+1, o);
+	fputc(0x00, o);
 
-    PBOENTRY e;
-    e.PackingMethod = 0;
-    e.OriginalSize = 0;
-    e.Reserved = 0;
-    e.TimeStamp = ft[i].timestamp;
-    e.DataSize = ft[i].len;
-    fwrite(&e, sizeof(PBOENTRY), 1, o);
-    //printf("file %d: %s\n", i, ft[i].fname);
+	PBOENTRY e;
+	e.PackingMethod = 0;
+	e.OriginalSize = 0;
+	e.Reserved = 0;
+	e.TimeStamp = ft[i].timestamp;
+	e.DataSize = ft[i].len;
+	fwrite(&e, sizeof(PBOENTRY), 1, o);
+	//printf("file %d: %s\n", i, ft[i].fname);
   }
   
   // Write blank separator block
@@ -504,27 +237,27 @@ bool pboPack(char *sd, char *df, bool overwrite) {
 
   // Write file data
   for(int i=0;i<fti;i++) {
-    printf("file %d/%d: %s (%d KB)\n", i, fti, ft[i].fname, ft[i].len/1024);
+	printf("file %d/%d: %s (%d KB)\n", i, fti, ft[i].fname, ft[i].len/1024);
 
-    FILE *inp = fopen(ft[i].fname, "rb");
-    if(!inp) {
-      printf("Warning! Cannot open file for reading!\n");
-      continue;
-    }
+	FILE *inp = fopen(ft[i].fname, "rb");
+	if(!inp) {
+	  printf("Warning! Cannot open file for reading!\n");
+	  continue;
+	}
 
-    // Read from file, write to pbo
-    #define BUFSIZE	1024*1024*4
-    char *buf = new char[BUFSIZE];
-    int w = ft[i].len;
-    while(w > 0) {
-      int read = fread(buf, 1, w>BUFSIZE?BUFSIZE:w, inp);
-      fwrite(buf, read, 1, o);
-      w-=BUFSIZE;			
-      sha1_update(&ctx, (uchar*) buf, read);
-    }
+	// Read from file, write to pbo
+	#define BUFSIZE	1024*1024*4
+	char *buf = new char[BUFSIZE];
+	int w = ft[i].len;
+	while(w > 0) {
+	  int read = fread(buf, 1, w>BUFSIZE?BUFSIZE:w, inp);
+	  fwrite(buf, read, 1, o);
+	  w-=BUFSIZE;			
+	  sha1_update(&ctx, (uchar*) buf, read);
+	}
 
-    delete[] buf; 
-    fclose(inp);
+	delete[] buf; 
+	fclose(inp);
   }
 
   // Write 0x00 + SHA-1 hash
@@ -532,72 +265,9 @@ bool pboPack(char *sd, char *df, bool overwrite) {
   BYTE sha1sum[20];
   sha1_finish(&ctx, sha1sum);
   fwrite(sha1sum, 20, 1, o);
-    
+	
   fclose(o);
   delete[] ft;
-
-  return true;
-}
-
-// PBO decompressor
-bool pboDecompress(BYTE *buf, BYTE *out, int size, int outSize) {
-  size = size-4;
-  DWORD *checksumCorrect = (DWORD*) &buf[size];
-
-  memset(out, 0, outSize);
-
-  BYTE *ptr = buf;
-  BYTE *optr = out;
-  // Read format bit
-  while ((ptr - buf) < size) {
-    char f = *ptr++;
-    for(int i=0;i<8;i++) {
-      if ((ptr - buf) > size) break;
-      if (optr-out > outSize) break;
-      if (f & 0x01) {
-        // Direct write byte
-        *optr++ = *ptr++;
-      } else {
-        BYTE *endofwindow = optr;
-        int data = 0;
-        data = (*(unsigned short*)ptr);
-        ptr += 2;
-        unsigned int rpos = (data & 0xff) + ((data & 0xf000) >> 4);
-        unsigned int rlen = ((data >> 8) & 0x0f) + 3;
-
-        BYTE *windowptr = optr - rpos;
-        BYTE *begofwindow;
-
-        if(rpos == 0)
-          begofwindow = windowptr-4096;
-        else
-          begofwindow = optr - rpos;
-
-        for (DWORD j = 0; j < rlen; j++) {
-          if(windowptr >= endofwindow) windowptr = begofwindow;
-          if(windowptr < out)
-            *optr++ = 0x20;
-          else
-            *optr++ = *windowptr;
-          windowptr++;
-        }
-      }
-      f = f >> 1;
-    }
-  }
-  //printf("Decompressed size: %db", optr-out);
-
-  // Calculate checksum
-  int checksum = 0;
-  int dlen = outSize;
-  for(DWORD i=0;i<(DWORD)dlen;i++)
-    checksum += (char)out[i];
-  
-  //printf("Checksum: %X - %X\n", checksum, checksumCorrect);
-  if(checksum != *checksumCorrect) {
-    printf("Decompression checksum mismatch! %08X != %08X\n", checksum, *checksumCorrect);
-    return false;
-  }
 
   return true;
 }
