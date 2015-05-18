337e70fd4ff70f46a4d4a1fae1e4bca772d2c8c8
ShuraDE <mail@kev-in.net>
2015-05-18 15:25:39 +0200

    add credits

--- CPBO/main.cpp
+++ CPBO/main.cpp
@@ -14,6 +14,9 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA  02110-1301  USA
+
+Modified by Gruppe Adler (www.gruppe-adler.de) - shura, fusselwurm
+* add linux compatibility
 */
 
 #include <iostream>
